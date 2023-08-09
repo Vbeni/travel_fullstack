fa8000f3036bc6360d1bf4e01f67916e4845d029
Vbeni <benivillafana@gmail.com>
2023-08-08 17:42:44 -0700

    Room model

--- main_app/models.py
+++ main_app/models.py
@@ -10,3 +10,14 @@ class Hotel(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Room(models.Model):
+    #links each room to a hotel 
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    room_type = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.room_type - self.hotel.name}"
+    
