c1d8c2c917bad736b89b03e2fb9363e5493428ef
Vbeni <benivillafana@gmail.com>
2023-08-08 17:44:42 -0700

    CustomUser model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Hotel(models.Model):
@@ -20,4 +21,10 @@ class Room(models.Model):
 
     def __str__(self):
         return f"{self.room_type - self.hotel.name}"
+    
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_lenght=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
     
