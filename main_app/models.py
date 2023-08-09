a2432a72d9a729a3346cc08661399f8200a89329
Vbeni <benivillafana@gmail.com>
2023-08-08 17:35:15 -0700

    Hotel model added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    description = models.TextField()
+    image_url = models.URLField()
+
+    def __str__(self):
+        return self.name
