3598b9bc324cfc66fce9e485f82fb2fa0e5b8f03
Vbeni <benivillafana@gmail.com>
2023-08-10 18:14:11 -0700

    added rest of models to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include 
 from rest_framework.routers import DefaultRouter
-from .views import HotelViewSet 
+from .views import HotelViewSet, RoomViewSet, CustomUserViewSet, BookingViewSet, ReviewViewSet
+
 
 router = DefaultRouter()
 router.register(r'hotels', HotelViewSet)
+router.register(r'rooms', RoomViewSet)
+router.register(r'users', CustomUserViewSet)
+router.register(r'bookings', BookingViewSet)
+router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
