aba82edfa6e6871c3195aa95f95cfa6172b971c7
Vbeni <benivillafana@gmail.com>
2023-08-10 18:10:29 -0700

    serializers finished

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,8 +1,31 @@
+from rest_framework import viewsets
 from rest_framework import serializers 
-from .models import Hotel
+from .models import Hotel, Room, Review, CustomUser, Booking
 
 class HotelSerializer(serializers):
     class Meta:
         model = Hotel
         fields = '__all__'
-    
+
+class RoomSerializer(serializers):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+class ReviewSerializer(serializers):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class BookingSerializer(serializers):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class CustomUserSerializer(serializers):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+
+
