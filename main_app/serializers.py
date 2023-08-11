8d3c6e0eb8cc0ac04d2c4ff1320c2ce41d97294e
Vbeni <benivillafana@gmail.com>
2023-08-10 18:30:55 -0700

    fixed serializers inheritance

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,28 +1,27 @@
-from rest_framework import viewsets
 from rest_framework import serializers 
 from .models import Hotel, Room, Review, CustomUser, Booking
 
-class HotelSerializer(serializers):
+class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
         fields = '__all__'
 
-class RoomSerializer(serializers):
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = '__all__'
 
-class ReviewSerializer(serializers):
+class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
 
-class BookingSerializer(serializers):
+class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
 
-class CustomUserSerializer(serializers):
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
